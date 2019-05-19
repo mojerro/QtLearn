7fd90817f28a303808cc76d22da083d73b701a62
Mojerro <mojerro@126.com>
2019-05-19 19:38:10 +0800

    add josephus circle gui

--- app/JosephusCircle.py
+++ app/JosephusCircle.py
@@ -1,6 +1,9 @@
 from ctypes import CDLL, c_char_p
 from utils.utils import convert_byte
-import os
+from UI.ui_josephus_circle import Ui_JosephusCircleFrame
+from PySide2.QtWidgets import QFrame, QApplication
+from PySide2.QtCore import SIGNAL, Slot
+import os, sys
 
 """
 types:
@@ -13,7 +16,8 @@ b'\x05\x01\x07\x04\x03\x06\t\x02\x08\x01\x10\xac\xef\x01'
 b'\x05\x01\x07\x04\x03\x06\t\x02\x08\x01\x10\xac\xef\x01'
 """
 
-path = os.path.join(os.getcwd(), r'build\JosephusCircle.so')
+# path = os.path.join(os.getcwd(), r'build\JosephusCircle.so')
+path = r'D:\Programming\QtLearn\build\JosephusCircle.so'
 josephus = CDLL(path)
 
 
@@ -27,3 +31,57 @@ def josephus_circle_linked_list(length: int, times: int, starter: int) -> list:
     res = josephus.JosephusCircleLinkedList
     res.restype = c_char_p
     return convert_byte(res(length, times, starter))[:length]
+
+
+class JosephusCircleWindow(QFrame, Ui_JosephusCircleFrame):
+
+    def __init__(self, main):
+        QFrame.__init__(self)
+        self.setupUi(self)
+        self.main = main
+        self.people = 99
+        self.starter = 1
+        self.times = 5
+        self.spinBox_times.setValue(self.times)
+        self.spinBox_starter.setValue(self.starter)
+        self.spinBox_people.setValue(self.people)
+        self.connect(self.spinBox_people, SIGNAL("valueChanged(int)"), self.set_people)
+        self.connect(self.spinBox_starter, SIGNAL("valueChanged(int)"), self.set_starter)
+        self.connect(self.spinBox_times, SIGNAL("valueChanged(int)"), self.set_times)
+        self.connect(self.commitButton, SIGNAL("clicked()"), self.run)
+
+    @Slot(int)
+    def set_people(self, val):
+        self.people = val
+
+    @Slot(int)
+    def set_starter(self, val):
+        self.starter = val
+
+    @Slot(int)
+    def set_times(self, val):
+        self.times = val
+
+    @Slot()
+    def run(self):
+        print(self.people, self.times, self.starter, self.comboBox.currentText())
+        if self.comboBox.currentText() == "数组":
+            res = josephus_circle_arr(self.people, self.times, self.starter)
+        elif self.comboBox.currentText() == "链表":
+            res = josephus_circle_linked_list(self.people, self.times, self.starter)
+        else:
+            raise NameError
+        print('res', res)
+        self.labelResult.setText(str(res))
+
+    def closeEvent(self, event=None):
+        if event is not None:
+            self.main.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = JosephusCircleWindow()
+    window.show()
+    # sys.exit(app.exec_())
+    app.exec_()
--- start.py
+++ start.py
@@ -3,7 +3,52 @@
     time:
 """
 from app.JosephusCircle import josephus_circle_arr, josephus_circle_linked_list
+import sys
+from PySide2 import QtCore, QtGui
+from PySide2.QtWidgets import QMainWindow, QApplication, QWidget
+from UI.ui_main import Ui_MainWindow
+from app.JosephusCircle import JosephusCircleWindow
+
+try:
+    # Python v2.
+    unicode
+
+
+    def encode_utf8(ba):
+        return unicode(ba, encoding='utf8')
+
+
+    def decode_utf8(qs):
+        return QtCore.QByteArray(str(qs))
+
+except NameError:
+    # Python v3.
+
+    def encode_utf8(ba):
+        return str(ba.data(), encoding='utf8')
+
+
+    def decode_utf8(qs):
+        return QtCore.QByteArray(bytes(qs, encoding='utf8'))
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.JosephusCircleButton.clicked.connect(self.click_josephus_circle)
+        self.frame = JosephusCircleWindow(self)
+
+    @QtCore.Slot()
+    def click_josephus_circle(self):
+        self.frame.show()
+        self.hide()
+
 
 if __name__ == '__main__':
-    print('linked list: ', josephus_circle_linked_list(9, 5, 1))
-    print('arr: ', josephus_circle_arr(9, 5, 1))
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    # sys.exit(app.exec_())
+    app.exec_()
